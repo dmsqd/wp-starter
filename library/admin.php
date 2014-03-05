1bd3280912715f59c6469762ebeb6092168a0b93
Edward Townend <ed@dmsqd.com>
2014-03-05 14:24:49 +0000

    Updated function for adding custom login CSS to proper enqueue method

--- library/admin.php
+++ library/admin.php
@@ -93,8 +93,7 @@ add_action('wp_dashboard_setup', 'bones_custom_dashboard_widgets');
 
 // calling your own login css so you can style it
 function bones_login_css() {
-	/* I couldn't get wp_enqueue_style to work :( */
-	echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/library/css/login.css">';
+	wp_enqueue_style( 'bones_login_css', get_template_directory_uri() . '/library/css/login.css', false );
 }
 
 // changing the logo link from wordpress.org to your site
@@ -104,7 +103,7 @@ function bones_login_url() {  return home_url(); }
 function bones_login_title() { return get_option('blogname'); }
 
 // calling it only on the login page
-add_action('login_head', 'bones_login_css');
+add_action( 'login_enqueue_scripts', 'bones_login_css', 10 );
 add_filter('login_headerurl', 'bones_login_url');
 add_filter('login_headertitle', 'bones_login_title');
 
