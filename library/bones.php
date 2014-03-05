f5434fee00c011be7d9dec484d183533751b8abb
Edward Townend <ed@dmsqd.com>
2014-03-05 14:57:39 +0000

    Fix 500 error

--- library/bones.php
+++ library/bones.php
@@ -374,7 +374,7 @@ function bones_filter_ptags_on_images($content){
 function bones_excerpt_more($more) {
 	global $post;
 	// edit here if you like
-	return '...  <a href="'. get_permalink($post->ID) . '" title="Read '.get_the_title($post->ID).'">Read more &raquo;</a>';
+	return '...  <a href="'. get_permalink($post->ID) . '" title="'. __('Read', 'dmsqdtheme') . get_the_title($post->ID).'">'. __('Read more &raquo;', 'dmsqdtheme') .'</a>';
 }
 
 
